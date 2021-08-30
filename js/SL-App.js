4fe6f2dfb1c64452bf86f3bab0ca296bce3bff2a
Samson <31880780+Zerueru90@users.noreply.github.com>
2021-08-30 16:54:17 +0200

    setinterval är klurig

--- js/SL-App.js
+++ js/SL-App.js
@@ -1,3 +1,7 @@
+// window.onload = function () {
+//     setInterval(Start, 60000);
+// };
+
 function creatingElement(element) 
 {
     return document.createElement(element);
@@ -12,85 +16,96 @@ function containsWord(str, word)
 {
     return str.match(new RegExp("\\b" + word + "\\b")) != null;
 }
-  
+
+let id;
+let stopInterval;
 
 function GetStationID(stationName)
 {
 
     const url = `https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/typeahead.json?key=494288bb92614e8cb19a001768f94f65&searchstring=${stationName.value}&stationsonly=true`;
-    var id;
+  
     //document.getElementById("test").innerHTML = url;
 
     fetch(url)
 
         .then((reps) => reps.json())
 
-        .then(function (data){
+            .then(function (data){
 
-            let stations = data.ResponseData;
+                let stations = data.ResponseData;
 
-            stations.map(function (station){
+                stations.map(function (station){
 
-                // if(containsWord(station.Name, stationName.value)) 
-                // {
-                //     //document.getElementById("test").innerHTML = station.Name;
-                //     Start(station.SiteId);
-                // }
-                if(station.Name === stationName.value) 
-                {
-                    id = station.SiteId;
-                }
+                    // if(containsWord(station.Name, stationName.value)) 
+                    // {
+                    //     //document.getElementById("test").innerHTML = station.Name;
+                    //     Start(station.SiteId);
+                    // }
+                    if(station.Name === stationName.value) 
+                    {
+                        id = station.SiteId;
+                    }
+
+                })
+
+                Start();
+                // stopInterval = setInterval(Start, 5000)
+
+                // document.getElementById("test").innerHTML = station;
 
             })
-
-            Start(id);
-            // document.getElementById("test").innerHTML = station;
-
-        })
 }
 
-function Start(trainID)
+//Flemingsbergs station (Huddinge)
+
+function Start()
 {
+    
     const first = document.getElementById("first");
     const second = document.getElementById("second");
     const third = document.getElementById("third");
-    const url = `https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=1c426b82dcf3493a9021f2db8e82717c&siteid=${trainID}&timewindow=30`;
+    const url = `https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=1c426b82dcf3493a9021f2db8e82717c&siteid=${id}&timewindow=10`;
 
     fetch(url)
 
         .then((resp) => resp.json())
 
-        .then(function (data){
-            
-            let trains = data.ResponseData.Trains;
+            .then(function (data){
+                
+                let trains = data.ResponseData.Trains;
 
-            trains.map(function (train)
-            {
-                let div = creatingElement('div'),
-                    lineNumber = creatingElement('div'),
-                    destinationName = creatingElement('div')
-                    time = creatingElement('div');
+                trains.map(function (train)
+                {
+                    let div = creatingElement('div'),
+                        lineNumber = creatingElement('div'),
+                        destinationName = creatingElement('div')
+                        time = creatingElement('div');
 
 
-                    
-                var newStr = train.ExpectedDateTime.replace('T', ' ');
-                var incomingDates = new Date(newStr);
-                var nowDate = new Date();
-                var minutesTime = diff_minutes(nowDate, incomingDates);
+                        
+                    var newStr = train.ExpectedDateTime.replace('T', ' ');
+                    var incomingDates = new Date(newStr);
+                    var nowDate = new Date();
+                    var minutesTime = diff_minutes(nowDate, incomingDates);
 
-                lineNumber.innerHTML = train.LineNumber;
-                destinationName.innerHTML = train.Destination;
-                time.innerHTML = minutesTime + " " + "min";
+                    lineNumber.innerHTML = train.LineNumber;
+                    destinationName.innerHTML = train.Destination;
+                    //time.innerHTML = minutesTime + " " + "min";
+                    time.innerHTML = train.DisplayTime;
 
-                document.getElementById("textDisplay-Top").innerHTML = train.StopAreaName;
+                    document.getElementById("textDisplay-Top").innerHTML = train.StopAreaName;
 
-                append(first, lineNumber);
-                append(second, destinationName);
-                append(third, time);
+                    append(first, lineNumber);
+                    append(second, destinationName);
+                    append(third, time);
+
+                })
+
+                setInterval(Start, 3000);
+                //clearInterval(stopInterval);
 
             })
-
-        })
 
         .catch(function (error){
             document.getElementById("dataDisplay-Text").innerHTML = error;
